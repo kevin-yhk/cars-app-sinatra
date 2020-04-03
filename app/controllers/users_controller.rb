47072697a698da93ea76d61272fd66ed94f0b94a
kevinkim <kevinkim.yhk@gmail.com>
2020-04-02 23:57:42 -0700

    fixed a minor type that was routing the post signup to a different directory

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
         end
     end 
 
-    post '/signup/' do
+    post '/signup' do
         @user = User.new(params)
         if @user.save 
             session[:user_id] = @user.id 
@@ -44,4 +44,13 @@ class UsersController < ApplicationController
             redirect to '/signup'
         end 
     end 
+
+    get '/logout' do
+        if logged_in?
+          session.clear
+          redirect to '/'
+        else
+          redirect to '/'
+        end
+    end
 end 
