b08fb9d2d034612f003894f108ea88e37f08c71e
kevinkim <kevinkim.yhk@gmail.com>
2020-04-02 23:33:55 -0700

    added get route for users id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     get '/login' do 
         erb :'/users/login'
     end 
+
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+           session[:user_id] = @user.id
+           redirect "users/#{@user.id}"
+        else
+           redirect '/login'
+        end
+     end
+
+     get '/users/:id' do
+        @user = User.find_by(id: params[:id])
+        erb :'/users/show'
+      end
 end 
