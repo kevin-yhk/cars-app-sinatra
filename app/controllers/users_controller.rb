bab504d072f5283e00177cc4cc5b36aef3a59c71
kevinkim <kevinkim.yhk@gmail.com>
2020-04-02 23:44:50 -0700

    added post route for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,14 @@ class UsersController < ApplicationController
       get '/signup' do
           erb :'/users/create'
       end
+
+      post '/signup/' do
+        @user = User.new(params)
+        if @user.save 
+            session[:user_id] = @user.id 
+            redirect "users/#{@user.id}"
+        else
+            redirect to '/signup'
+        end 
+    end 
 end 
