3f04960cdf1c8381aaebb849133772b1ee308fd7
kevinkim <kevinkim.yhk@gmail.com>
2020-04-03 18:15:00 -0700

    added validations for uniqueness to avoid bad duplicate data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
     has_many :cars
     has_secure_password
+
+    validates :username, :email, uniqueness: true
+    validates :username, :email, :password, presence: true
 end 
