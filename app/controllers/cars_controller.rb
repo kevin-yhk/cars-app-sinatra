19c04d38367c361271a933a4609385c30125d79c
kevinkim <kevinkim.yhk@gmail.com>
2020-04-03 20:20:22 -0700

    created a edit route for cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -44,6 +44,40 @@ class CarsController < ApplicationController
         end
     end
 
+    get '/cars/:id/edit' do
+        if logged_in?
+          @car = Car.find_by_id(params[:id])
+          if @car && @car.user == current_user
+            erb :'cars/edit'
+          else
+            redirect '/cars'
+          end
+        else
+          redirect '/login'
+        end
+    end
+
+    patch '/sentences/:id' do
+        if logged_in?
+          if params[:make] == "" || params[:model] == "" || params[:year] == "" || params[:trim] == ""
+            redirect to "/cars/#{params[:id]}/edit"
+          else
+            @car = Car.find_by_id(params[:id])
+            if @car && @car.user == current_user
+              if @car.update(make: params[:make], model: params[:model], year: params[:year], trim: params[:trim])
+                redirect to "/cars/#{@car.id}"
+              else
+                redirect "/cars/#{@car.id}/edit"
+              end
+            else
+              redirect '/cars'
+            end
+          end
+        else
+          redirect '/login'
+        end
+    end
+
 
 
 
